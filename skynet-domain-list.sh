da513c92a45d6740384120b758cb4aafcaaf732f
Mustafa Emek <9492057+emekm@users.noreply.github.com>
2025-07-22 23:43:42 +0200

    Update skynet-domain-list.sh

--- skynet-domain-list.sh
+++ skynet-domain-list.sh
@@ -1,41 +1,39 @@
 #!/bin/sh
 
-# Domains to whitelist (space‑separated string)
+# Liste des domaines à whitelist (séparés par espaces)
 WHITELIST_DOMAINS="line.diatunnel.link"
 
-# Domains to blacklist (empty for now)
+# Liste des domaines à blacklist (vide pour l'instant)
 BLACKLIST_DOMAINS=""
 
-# Function to whitelist
 whitelist_domain() {
-    DOMAIN="$1"
-    IP=$(ping -c 1 "$DOMAIN" | awk -F'[()]' '/PING/{print $2}')
+    DOMAIN=$1
+    IP=$(ping -c 1 $DOMAIN | awk -F'[()]' '/PING/{print $2}')
     if [ -n "$IP" ]; then
         logger -t Skynet-Whitelist "Whitelisting $DOMAIN ($IP)"
-        /jffs/scripts/firewall whitelist add "$IP"
+        /jffs/scripts/firewall whitelist add $IP
     else
         logger -t Skynet-Whitelist "Failed to resolve $DOMAIN"
     fi
 }
 
-# Function to blacklist
 blacklist_domain() {
-    DOMAIN="$1"
-    IP=$(ping -c 1 "$DOMAIN" | awk -F'[()]' '/PING/{print $2}')
+    DOMAIN=$1
+    IP=$(ping -c 1 $DOMAIN | awk -F'[()]' '/PING/{print $2}')
     if [ -n "$IP" ]; then
         logger -t Skynet-Blacklist "Blacklisting $DOMAIN ($IP)"
-        /jffs/scripts/firewall blacklist add "$IP"
+        /jffs/scripts/firewall blacklist add $IP
     else
         logger -t Skynet-Blacklist "Failed to resolve $DOMAIN"
     fi
 }
 
-# Process whitelist
-for DOMAIN in $WHITELIST_DOMAINS; do
-    whitelist_domain "$DOMAIN"
+for DOMAIN in $WHITELIST_DOMAINS
+do
+    whitelist_domain $DOMAIN
 done
 
-# Process blacklist
-for DOMAIN in $BLACKLIST_DOMAINS; do
-    blacklist_domain "$DOMAIN"
+for DOMAIN in $BLACKLIST_DOMAINS
+do
+    blacklist_domain $DOMAIN
 done
